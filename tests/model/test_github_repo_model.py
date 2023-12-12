caaab7d67b254b37346c224e6bc7a7007f2e3968
Leonardo Baptista <leonardoobaptistaa@gmail.com>
2023-12-12 20:42:12 -0300

    Add tests to github_repo_model

--- tests/model/test_github_repo_model.py
+++ tests/model/test_github_repo_model.py
@@ -1,5 +1,27 @@
 from model import GithubRepo
 
 
-async def test_1_equal_1():
-    assert 1 == 1
+async def test_save_find_all():
+    repo = GithubRepo(id=None, owner="leonardoobaptistaa", name="repo1", likes=100)
+    await repo.save()
+
+    all_repos = await GithubRepo.find_all()
+
+    assert 1 == len(all_repos)
+    repo = all_repos[0]
+    assert "leonardoobaptistaa" == repo.owner
+    assert "repo1" == repo.name
+    assert 100 == repo.likes
+    assert repo.id is not None
+
+
+async def test_statistics(make_github_repo):
+    await make_github_repo(owner="aaa", likes=1)
+    await make_github_repo(owner="aaa", likes=2)
+    await make_github_repo(owner="aaa", likes=2)
+    await make_github_repo(owner="aaa", likes=2)
+    await make_github_repo(owner="aaa", likes=3)
+
+    statistics = await GithubRepo.statistics("aaa")
+
+    assert statistics == (10, 2.0, 5)
