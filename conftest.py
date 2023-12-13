a5a51c4b404029a8e05ef7e65f129ee17b4ac8a9
Leonardo Baptista <leonardoobaptistaa@gmail.com>
2023-12-12 21:09:19 -0300

    Fix conftest

--- conftest.py
+++ conftest.py
@@ -1,8 +1,24 @@
+import asyncio
+
 import aiosqlite
 import pytest
 
 from config.db import connection_string as conn
+from config.db import init_db
 from model import GithubRepo
+
+
+@pytest.fixture(scope="session", autouse=True)
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def create_db():
+    await init_db()
+    yield
 
 
 @pytest.fixture(autouse=True)
@@ -13,15 +29,15 @@ async def _clear_database():
 
 async def clear_db():
     async with aiosqlite.connect(conn()) as db:
-        # await db.execute("DELETE FROM github")
-        # await db.commit()
-        pass
+        await db.execute("DELETE FROM github")
+        await db.commit()
 
 
 @pytest.fixture
 async def make_github_repo():
     async def _make_github_repo(**params):
         default_values = {
+            "id": None,
             "owner": "onwer_de_teste",
             "name": "repositorio de teste",
             "likes": 10,
